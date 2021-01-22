dd36d57645f93a448f6cd4811ee300b60b95253a
Monique <momo_89@ymail.com>
2021-01-22 11:31:38 -0600

    commit tree 7bc32a044c2bb31bce6a5cb35f5f6de0b1b2cf9d

--- grandma.rb
+++ grandma.rb
@@ -10,4 +10,24 @@
 # NO, NOT SINCE 1938!
 
 # However if you say 'I LOVE YOU GRANDMA!', she should respond with
-# 'I LOVE YOU TOO PUMPKIN!'
+# 'I LOVE YOU TOO PUMPKIN'
+
+def speak_to_grandma(speak)
+    if speak == "Hi Nana, how are you?"
+        "HUH?! SPEAK UP, SONNY!".upcase
+    elsif speak == "I LOVE YOU GRANDMA!".upcase
+        "I LOVE YOU TOO PUMPKIN!".upcase
+    elsif speak == "WHAT DID YOU EAT TODAY?".upcase
+        "NO, NOT SINCE 1938!".upcase
+    elsif speak == "Hi!"
+        "HUH?! SPEAK UP, SONNY!".upcase
+    elsif speak = "WHAT?".upcase
+        "NO, NOT SINCE 1938!".upcase
+    else
+        "HUH?! SPEAK UP, SONNY!".upcase
+    end
+end
+speak_to_grandma("Hi Nana, how are you?")
+speak_to_grandma("I LOVE YOU GRANDMA!")
+speak_to_grandma("WHAT DID YOU EAT TODAY?")
+speak_to_grandma("Hi")
